3156a2ef542c41153b4d19a88b1b6bac6adff196
xulei <https://github.com/caolong124566/lingyun.git>
2018-01-04 15:19:48 +0800

    默认的

--- src/main/java/com/lingyun/framework/entity/Employee.java
+++ src/main/java/com/lingyun/framework/entity/Employee.java
@@ -4,24 +4,14 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-<<<<<<< HEAD
 @Table(name="Employee")
-=======
-@Table(name = "employee")
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;//主键   无意义
-<<<<<<< HEAD
     @Column(length = 50,unique = true,nullable = false)
     private String number;//员工编号 唯一值
     @Column(length = 20,nullable = false,unique = true)
-=======
-    @Column(length = 50,nullable = false)
-    private String number;//员工编号 唯一值
-    @Column(length = 20,nullable = false)
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
     private String telephone;//注册时的手机号码 唯一值
     @Column(length = 30,nullable = false)
     private String name;//用户昵称
@@ -31,21 +21,10 @@ public class Employee {
     private String sex;//用户性别  默认为男
     @Column(length = 30,nullable = false)
     private Date brithday;//出生年月日
-<<<<<<< HEAD
     @Column(length = 10,nullable = false)
     private Integer positionId;//职位ID  为外键
     @Column(length = 30,nullable = false)
     private Integer departMentId;//部门ID  为外键
-=======
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "posID")
-    private Position positionId;//职位ID  为外键
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "depID")
-    private Department departMentId;//部门ID  为外键
-
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
     @Column(length = 20,nullable = false)
     private String nation;//民族 不能为空
     @Column(length = 20,nullable = false)
@@ -60,20 +39,10 @@ public class Employee {
     @Column(nullable = false)
     private Date entryDate;//入职时间 不能为空
     @Column(nullable = false)
-<<<<<<< HEAD
     private String maritalStatus;//婚姻状况ID 外键
     @Column(nullable = false)
     private String photoUrl;//照片地址
 
-=======
-    private Integer maritalStatus;//婚姻状况ID 外键
-    @Column(nullable = false)
-    private String photoUrl;//照片地址
-
-    public Employee() {
-        super();
-    }
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
 
     public Integer getId() {
         return id;
@@ -91,17 +60,6 @@ public class Employee {
         this.number = number;
     }
 
-<<<<<<< HEAD
-=======
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
     public String getName() {
         return name;
     }
@@ -130,7 +88,6 @@ public class Employee {
         return brithday;
     }
 
-<<<<<<< HEAD
     public void setBrithday(Date brithday) {
         this.brithday = brithday;
     }
@@ -148,21 +105,6 @@ public class Employee {
     }
 
     public void setDepartMentId(Integer departMentId) {
-=======
-    public Position getPositionId() {
-        return positionId;
-    }
-
-    public void setPositionId(Position positionId) {
-        this.positionId = positionId;
-    }
-
-    public Department getDepartMentId() {
-        return departMentId;
-    }
-
-    public void setDepartMentId(Department departMentId) {
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
         this.departMentId = departMentId;
     }
 
@@ -190,7 +132,6 @@ public class Employee {
         this.salary = salary;
     }
 
-<<<<<<< HEAD
     public String getTelephone() {
         return telephone;
     }
@@ -199,8 +140,6 @@ public class Employee {
         this.telephone = telephone;
     }
 
-=======
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
     public String getAddress() {
         return address;
     }
@@ -217,13 +156,6 @@ public class Employee {
         this.email = email;
     }
 
-<<<<<<< HEAD
-=======
-    public void setBrithday(Date brithday) {
-        this.brithday = brithday;
-    }
-
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
     public Date getEntryDate() {
         return entryDate;
     }
@@ -232,19 +164,11 @@ public class Employee {
         this.entryDate = entryDate;
     }
 
-<<<<<<< HEAD
     public String getMaritalStatus() {
         return maritalStatus;
     }
 
     public void setMaritalStatus(String maritalStatus) {
-=======
-    public Integer getMaritalStatus() {
-        return maritalStatus;
-    }
-
-    public void setMaritalStatus(Integer maritalStatus) {
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
         this.maritalStatus = maritalStatus;
     }
 
@@ -256,31 +180,6 @@ public class Employee {
         this.photoUrl = photoUrl;
     }
 
-<<<<<<< HEAD
 
 
-=======
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", number='" + number + '\'' +
-                ", telephone='" + telephone + '\'' +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", sex='" + sex + '\'' +
-                ", brithday=" + brithday +
-                ", positionId=" + positionId +
-                ", departMentId=" + departMentId +
-                ", nation='" + nation + '\'' +
-                ", nativePlace='" + nativePlace + '\'' +
-                ", salary=" + salary +
-                ", address='" + address + '\'' +
-                ", email='" + email + '\'' +
-                ", entryDate=" + entryDate +
-                ", maritalStatus=" + maritalStatus +
-                ", photoUrl='" + photoUrl + '\'' +
-                '}';
-    }
->>>>>>> 3d46f6cd65bd57d1e198eefc4d9f71d5547b43d8
 }
